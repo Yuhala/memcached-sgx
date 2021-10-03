eba4e5e115860d38b65f94aeebbc3a5c89236076
Yuhala <petersonyuhala@gmail.com>
2021-10-03 12:59:53 +0200

    modified implementation of switchless cache: seems OK

--- sgx/Enclave/zcTrusted/zc_lfu.cpp
+++ sgx/Enclave/zcTrusted/zc_lfu.cpp
@@ -4,132 +4,32 @@
  * Copyright (c) 2021 Peterson Yuhala, IIUN
  * 
  * ZC switchless cache implementation; follows a least frequently used algorithm to replace routines in the switchless cache.
- * Based on lfu cache implem: https://www.geeksforgeeks.org/least-frequently-used-lfu-cache-implementation/
+ * Revision on LFU : https://www.geeksforgeeks.org/least-frequently-used-lfu-cache-implementation/
  * 
  */
 
-// C++ program for LFU cache implementation
-
 #include "zc_lfu.h"
 #include "Enclave.h"
+
+//forward declarations
+static int is_in_cache(std::vector<std::pair<int, int>> &v, zc_routine func_name);
+
 /**
  * This vector is ordered in the form of a min heap and contains a function id
  * as well as the number of times the corresponding function has been called.
  */
-std::vector<std::pair<int, int>> zc_switchless_cache(ZC_LFU_CACHE_SIZE);
+std::vector<std::pair<int, int>> zc_switchless_cache;
+
 /**
  * This is a hashmap which contains indices of all functions of the shim library which have been called
  * since the start of the enclave, as well as the number of times each function was called.
  */
 std::unordered_map<int, int> zc_map;
+std::unordered_map<int, int> cache_map; /* map for routines already in cache */
 
 int curr_cache_size = 0; /* number of items in the cache */
 
 int number_of_ocalls = 0; /* total number of ocalls/shim function calls since enclave start */
-
-// Generic function to swap two pairs
-void swap(std::pair<int, int> &a, std::pair<int, int> &b)
-{
-    std::pair<int, int> temp = a;
-    a = b;
-    b = temp;
-}
-
-// Returns the index of the parent node
-inline int parent(int i)
-{
-    return (i - 1) / 2;
-}
-
-// Returns the index of the left child node
-inline int left(int i)
-{
-    return 2 * i + 1;
-}
-
-// Returns the index of the right child node
-inline int right(int i)
-{
-    return 2 * i + 2;
-}
-
-// Self made heap tp Rearranges
-//  the nodes in order to maintain the heap property
-void heapify(std::vector<std::pair<int, int>> &v,
-             std::unordered_map<int, int> &m, int i, int n)
-{
-    int l = left(i), r = right(i), minim;
-    if (l < n)
-        minim = ((v[i].second < v[l].second) ? i : l);
-    else
-        minim = i;
-    if (r < n)
-        minim = ((v[minim].second < v[r].second) ? minim : r);
-    if (minim != i)
-    {
-        m[v[minim].first] = i;
-        m[v[i].first] = minim;
-        swap(v[minim], v[i]);
-        heapify(v, m, minim, n);
-    }
-}
-void increment(std::vector<std::pair<int, int>> &v,
-               std::unordered_map<int, int> &m, int i, int n)
-{
-    ++v[i].second;
-    heapify(v, m, i, n);
-}
-
-// Function to Insert a new node in the heap
-void insert(std::vector<std::pair<int, int>> &v,
-            std::unordered_map<int, int> &m, int value, int &n)
-{
-
-    if (n == v.size())
-    {
-        m.erase(v[0].first);
-#ifdef ZC_CACHE_TEST
-        printf("---------------------zc function: %d removed ------------------\n", v[0].first);
-#endif
-        v[0] = v[--n];
-        heapify(v, m, 0, n);
-    }
-    v[n++] = std::make_pair(value, 1);
-    m.insert(std::make_pair(value, n - 1));
-    int i = n - 1;
-
-    // Insert a node in the heap by swapping elements
-    while (i && v[parent(i)].second > v[i].second)
-    {
-        m[v[i].first] = parent(i);
-        m[v[parent(i)].first] = i;
-        swap(v[i], v[parent(i)]);
-        i = parent(i);
-    }
-#ifdef ZC_CACHE_TEST
-    printf("---------------------zc function: %d inserted ------------------\n", value);
-#endif
-}
-
-// Function to refer to the block/function value in the cache
-void refer(std::vector<std::pair<int, int>> &cache, std::unordered_map<int, int> &indices, int value, int &cache_size)
-{
-    if (indices.find(value) == indices.end())
-        insert(cache, indices, value, cache_size);
-    else
-        increment(cache, indices, indices[value], cache_size);
-}
-
-/**
- * Initialize all routine call counts in hashmap as 0
- */
-void init_zc_map()
-{
-    /*for (int i = ZC_FREAD; i < ZC_LAST; i++)
-    {
-        zc_map[i] = 0;
-    }*/
-}
 
 /**
  * This function tests if a function should be called as zc switchless or not; 
@@ -138,6 +38,8 @@ void init_zc_map()
 bool use_zc_switchless(zc_routine func_name)
 {
     number_of_ocalls++;
+    int f = (int)func_name;
+    bool test = false; /* true if func is inserted in cache ==> switchless hit; otherwise switchless miss */
 
     /**
      * pyuhala: maybe we should let the system "initialize for abit"
@@ -152,23 +54,116 @@ bool use_zc_switchless(zc_routine func_name)
     /**
      * Update switchless cache && function map
      */
-    refer(zc_switchless_cache, zc_map, (int)func_name, curr_cache_size);
-
-    /**
-     * check for switchless hit/miss (ie is fxn in cache or not)
-     */
-
-    bool test = false;
-    for (int i = 0; i < zc_switchless_cache.size(); i++)
+    if (zc_map.find(f) == zc_map.end())
     {
-        if (zc_switchless_cache[i].first == (int)func_name)
-        {
-            test = true;
-            break;
-        }
+        zc_map[f] = 1;
+    }
+    else
+    {
+        zc_map[f]++;
     }
 
+    test = cache_insert(zc_switchless_cache, func_name);
+
     return test;
+}
+
+static int is_in_cache(std::vector<std::pair<int, int>> &v, zc_routine func_name)
+{
+    int f = (int)func_name;
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i].first == f)
+        {
+            return i;
+        }
+    }
+    return NOT_IN_CACHE;
+}
+
+/**
+ * Try to insert a value in the cache
+ */
+
+bool cache_insert(std::vector<std::pair<int, int>> &v, zc_routine func_name)
+{
+    /**
+     * check if function is already in cache
+     */
+    int index = is_in_cache(v, func_name);
+    if (index != NOT_IN_CACHE)
+    {
+        //printf(" ----------------------- function %d already in cache ------------------\n", (int)func_name);
+        v[index].second++;
+        return true;
+    }
+
+    int f = (int)func_name;
+    /* it should be in the map already */
+    int count = zc_map[f];
+    bool inserted = false;
+
+    /* inserting a cache entry */
+    if (v.size() < ZC_LFU_CACHE_CAPACITY)
+    {
+        //printf("--curr cache size: %d----\n", curr_cache_size);
+
+        std::pair<int, int> entry = std::make_pair(f, count);
+        //v.insert causing some issues, no time to waste ..
+        v.push_back(entry);
+        curr_cache_size++;
+        //v[curr_cache_size++] = entry;
+
+#ifdef ZC_CACHE_TEST
+        printf("---------------------zc function: %d inserted ------------------\n", f);
+#endif
+        return true;
+    }
+
+    /* replacing a cache entry */
+
+    int minIndex = findLeastFrequent(v);
+    if (v[minIndex].second <= count)
+    {
+#ifdef ZC_CACHE_TEST
+        printf("---------------------zc function: %d removed ------------------\n", v[minIndex].first);
+        printf("---------------------zc function: %d inserted ------------------\n", f);
+
+#endif
+        std::pair<int, int> entry = std::make_pair(f, count);
+        // replace cache entry at min index
+        v[minIndex] = entry;
+        inserted = true;
+    }
+
+    return inserted;
+}
+
+int findLeastFrequent(std::vector<std::pair<int, int>> &v)
+{
+    /**
+     * This function is Ok when cache size is small.
+     * For larger cache size use a min-heap which is more efficient
+     * to track the smallest element
+     */
+    int minIndex = 0;
+    for (int i = 0; i < v.size(); i++)
+    {
+        if (v[i].second < v[minIndex].second)
+        {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+/**
+ * Initialize all routine call counts in hashmap as 0
+ */
+void clear_zc_cache()
+{
+    //TODO
 }
 
 void use_zc_test()
--- sgx/Enclave/zcTrusted/zc_lfu.h
+++ sgx/Enclave/zcTrusted/zc_lfu.h
@@ -20,24 +20,17 @@
 #include <map>
 #include <utility>
 
-#define ZC_LFU_CACHE_SIZE 2 /* max number of routines which will be zc switchless */
+#define ZC_LFU_CACHE_CAPACITY 2 /* max number of routines which will be zc switchless */
 #define ZC_LFU_INIT_NUM 0  /* number of ocalls done since the start of enclave b4 zc switchless is activated */
 
-//#define ZC_CACHE_TEST
+#define NOT_IN_CACHE -1
+#define ZC_CACHE_TEST
 
-void swap(std::pair<int, int> &a, std::pair<int, int> &b);
-void heapify(std::vector<std::pair<int, int>> &v,
-             std::unordered_map<int, int> &m, int i, int n);
-void increment(std::vector<std::pair<int, int>> &v,
-               std::unordered_map<int, int> &m, int i, int n);
 
-void insert(std::vector<std::pair<int, int>> &v,
-            std::unordered_map<int, int> &m, int value, int &n);
-
-void refer(std::vector<std::pair<int, int>> &cache, std::unordered_map<int, int> &indices, int value, int &cache_size);
-void init_zc_map();
-bool use_zc_switchless();
-
+bool use_zc_switchless(zc_routine func_name);
+bool cache_insert(std::vector<std::pair<int, int>> &v, zc_routine func_name);
+int findLeastFrequent(std::vector<std::pair<int, int>> &v);
+void clear_zc_cache();
 //test
 void use_zc_test();
 
